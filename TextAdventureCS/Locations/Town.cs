b752d5573842a16d0a6f52ad9d283ac26802935f
Justnuked <thommartens14@hotmail.com>
2015-02-02 11:17:49 +0100

    Added town base class

---
+++ TextAdventureCS/Locations/Town.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS.Locations
+{
+    class Town : Location
+    {
+        public Town(string name)
+            : base(name)
+        {
+ 
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine("You are standing in a village");
+        }
+    }
+}
